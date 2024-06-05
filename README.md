ae62bc09c75fd2ceb2820c99bbc39e0a8beed0e3
jchillah <jchillah@gmail.com>
2024-06-05 11:09:47 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ts_5_8_1_checklisten_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/ts_5_8_1_checklisten_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ts_5_8_1_checklisten_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
